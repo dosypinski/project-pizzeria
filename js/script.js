695dade59fd5188e8991bd94d8e15b77533ed40a
Damian Osypinski <d.osypinski@gmail.com>
2021-11-04 19:26:58 +0100

    add script result

--- js/script.js
+++ js/script.js
@@ -22,10 +22,49 @@ let playerMove = 'nieznany ruch';
 
 if(playerInput == '1'){
   playerMove = 'kamień';
-} else if (playerInput == '2') {
-  playerMove = 'nożyce';
 } else if (playerInput == '3') {
+  playerMove = 'nożyce';
+} else if (playerInput == '2') {
   playerMove = 'papier'
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+}
+if( computerMove == 'kamień' && playerMove == 'nożyce'){
+  printMessage('Ty przegrywasz!');
+}
+if( computerMove == 'kamień' && playerMove == 'kamień'){
+  printMessage('Mamy remis!');
+}
+if( computerMove == 'kamień' && playerMove == 'nieznany ruch'){
+  printMessage('Tylko 1,2,3!');
+}
+
+if( computerMove == 'papier' && playerMove == 'nożyce'){
+  printMessage('Ty wygrywasz!');
+}
+if( computerMove == 'papier' && playerMove == 'kamień'){
+  printMessage('Ty przegrywasz!');
+}
+if( computerMove == 'papier' && playerMove == 'papier'){
+  printMessage('Mamy remis!');
+}
+if( computerMove == 'papier' && playerMove == 'nieznany ruch'){
+  printMessage('Tylko 1,2,3!');
+}
+
+if( computerMove == 'nożyce' && playerMove == 'kamień'){
+  printMessage('Ty wygrywasz!');
+}
+if( computerMove == 'nożyce' && playerMove == 'papier'){
+  printMessage('Ty przegrywasz!');
+}
+if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+  printMessage('Mamy remis!');
+}
+if( computerMove == 'nożyce' && playerMove == 'nieznany ruch'){
+  printMessage('Tylko 1,2,3!');
+}
