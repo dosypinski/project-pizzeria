909d82eaf8472a578778e2b1a0ba90abb3ea0485
Damian Osypinski <d.osypinski@gmail.com>
2021-11-30 14:47:28 +0100

    modify processOrder

--- src/js/script.js
+++ src/js/script.js
@@ -89,6 +89,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
     
     initAccordion(){
@@ -161,8 +162,9 @@
           if(paramId.includes(option)){
             console.log('Checked');
           }
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
           // check if there is param with a name of paramId in formData and if it includes optionId
-          if(formData[paramId] && formData[paramId].includes(optionId)) {
+          if(optionSelected) {
             // check if the option is not default
             if(option.default != true) {
               // add option price to price variable
@@ -173,6 +175,18 @@
             if(option.default == true) {
               // reduce price variable
               price = price - option.price;
+            }
+          }
+
+          const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
+          console.log ('optionImage', optionImage);
+
+          if(optionImage) {
+            if(optionSelected){
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+            }
+            else {
+              optionImage.classList.remove(classNames.menuProduct.imageVisible);
             }
           }
         }
