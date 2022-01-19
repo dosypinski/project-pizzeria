0ddc420e5bad6d61efa417b9704c819b9336b91d
Damian Osypinski <d.osypinski@gmail.com>
2022-01-19 18:35:31 +0100

    add Booking class

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,18 @@
 import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
+
+  initBooking: function(){
+    const thisApp = this;
+
+    thisApp.booking = document.querySelector(select.containerOf.booking);
+
+    new Booking(thisApp.booking);
+  },
+
   initPages: function(){
     const thisApp = this;
 
@@ -105,6 +115,7 @@ const app = {
     thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initBooking();
   },
 };
   
